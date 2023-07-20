20264a22f029bb24a41c3bb9741b465f74a9a765
Andrew Kirov <AndrewKirov@me.com>
2023-07-20 23:02:00 +0500

    minor edit pytest/routes

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -34,6 +34,22 @@ def test_pages_availability_for_auth_user(author_client, name, comment):
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
+    'name, pk_for_comment',
+    (
+        ('news:delete', pytest.lazy_fixture('pk_comment')),
+        ('news:edit', pytest.lazy_fixture('pk_comment'))
+    )
+)
+def test_redirects(client, name, pk_for_comment):
+    login_url = reverse('users:login')
+    url = reverse(name, args=pk_for_comment)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
     'parametrized_client, expected_status',
     (
         (pytest.lazy_fixture('admin_client'), HTTPStatus.NOT_FOUND),
@@ -51,19 +67,3 @@ def test_pages_availability_for_different_users(parametrized_client, name, comme
     url = reverse(name, args=(comment.pk,))
     response = parametrized_client.get(url)
     assert response.status_code == expected_status
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    'name, pk_for_comment',
-    (
-        ('news:delete', pytest.lazy_fixture('pk_comment')),
-        ('news:edit', pytest.lazy_fixture('pk_comment'))
-    )
-)
-def test_redirects(client, name, pk_for_comment):
-    login_url = reverse('users:login')
-    url = reverse(name, args=pk_for_comment)
-    expected_url = f'{login_url}?next={url}'
-    response = client.get(url)
-    assertRedirects(response, expected_url)
